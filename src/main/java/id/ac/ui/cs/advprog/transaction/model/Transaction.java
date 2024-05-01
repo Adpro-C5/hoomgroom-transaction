9561760a2edd14b13add55b204eb611e08c84f6d
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-05-01 09:01:18 +0700

    [REFACTOR] use JPA, change paymentDate creation, add userId in Transaction model

--- src/main/java/id/ac/ui/cs/advprog/transaction/model/Transaction.java
+++ src/main/java/id/ac/ui/cs/advprog/transaction/model/Transaction.java
@@ -1,44 +1,56 @@
 package id.ac.ui.cs.advprog.transaction.model;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.util.Date;
 
+@Entity
+@Table(name="transaction")
 @Getter
 @Setter
 public class Transaction {
-    private String userUsername;
-    private UUID id;
+
+    @Id
+    @Column(name = "transaction_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID transactionId;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "product_id", nullable = false)
     private UUID productId;
+
+    @Column(name = "product_amount", nullable = false)
     private Integer productAmount;
+
+    @Column(name = "promo_code")
     private String promoCode;
+
+    @Column(name = "payment_date", nullable = false)
+    @CreationTimestamp
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
     private Date paymentDate;
+
+    @Column(name = "total_price")
     private Double totalPrice;
 
-    public Transaction(String userUsername){
-        this.userUsername = userUsername;
-        this.id = UUID.randomUUID();
-        this.productId = null;
-        this.productAmount = null;
-        this.promoCode = null;
-        this.paymentDate = null;
-        this.totalPrice = null;
-    }
+    public Transaction(){}
 
-    public Transaction(String userUsername,
-                       UUID productId,
-                       Integer productAmount,
-                       String promoCode,
-                       Date paymentDate,
-                       Double totalPrice){
-        this.userUsername = userUsername;
-        this.id = UUID.randomUUID();
+    public Transaction(
+            UUID userId,
+            UUID productId,
+            Integer productAmount,
+            String promoCode) {
+
+        this.userId = userId;
         this.productId = productId;
         this.productAmount = productAmount;
         this.promoCode = promoCode;
-        this.paymentDate = paymentDate;
-        this.totalPrice = totalPrice;
     }
-
 }
