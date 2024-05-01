a61f34ddda40377fd4a5e2d7d4a8188f98e7f860
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-05-01 11:52:26 +0700

    [REFACTOR] use DataJpaTest for transaction repository test

--- src/test/java/id/ac/ui/cs/advprog/transaction/repository/TransactionRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/transaction/repository/TransactionRepositoryTest.java
@@ -4,68 +4,66 @@ import id.ac.ui.cs.advprog.transaction.model.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.text.SimpleDateFormat;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
+@DataJpaTest
 class TransactionRepositoryTest{
 
     SimpleDateFormat dateFormat;
-    @InjectMocks
+
+    @Autowired
     TransactionRepository transactionRepository;
 
     @BeforeEach
     void setUp(){
+        // transactionRepository = Mockito.mock(TransactionRepository.class);
         this.dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
     }
 
     @Test
     void testCreateAndFind(){
-        Transaction transaction = new Transaction("dummyUsername");
-        transaction.setId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+        Transaction transaction = new Transaction();
+        transaction.setUserId(UUID.fromString("14ed324a-7d0b-45a5-8901-37e80a611f8c"));
         transaction.setProductId(UUID.fromString("658e3ed6-1a43-43b8-98f0-2d23421f65c8"));
         transaction.setProductAmount(15);
         transaction.setPromoCode(null);
-
-        try {
-            transaction.setPaymentDate(this.dateFormat.parse("Mon Apr 22 18:58:25 WIB 2024"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         transaction.setTotalPrice(34500000.0);
 
-        transactionRepository.create(transaction);
+        transactionRepository.save(transaction);
 
-        Iterator<Transaction> transactionIterator = transactionRepository.findAll();
-        assertTrue(transactionIterator.hasNext());
-        Transaction savedTransaction = transactionIterator.next();
-        assertEquals(transaction.getId(), savedTransaction.getId());
-        assertEquals(transaction.getUserUsername(), savedTransaction.getUserUsername());
+        List<Transaction> transactionList = transactionRepository.findAll();
+        Transaction savedTransaction = transactionList.getFirst();
+
+        assertNotEquals(null, savedTransaction.getTransactionId());
+        assertEquals(transaction.getUserId(), savedTransaction.getUserId());
         assertEquals(transaction.getProductId(), savedTransaction.getProductId());
         assertEquals(transaction.getProductAmount(), savedTransaction.getProductAmount());
         assertEquals(transaction.getPromoCode(), savedTransaction.getPromoCode());
-        assertEquals(transaction.getPaymentDate(), savedTransaction.getPaymentDate());
+        assertNotEquals(null, savedTransaction.getPaymentDate());
         assertEquals(transaction.getTotalPrice(), savedTransaction.getTotalPrice());
     }
 
     @Test
     void testFindAllIfEmpty(){
-        Iterator<Transaction> transactionIterator = transactionRepository.findAll();
-        assertFalse(transactionIterator.hasNext());
+        List<Transaction> transactionList = transactionRepository.findAll();
+        assertTrue(transactionList.isEmpty());
     }
 
     @Test
     void testEditTransaction(){
-        Transaction transaction = new Transaction("dummyUsername");
-        transaction.setId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+        Transaction transaction = new Transaction();
+        transaction.setTransactionId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+        transaction.setUserId(UUID.fromString("14ed324a-7d0b-45a5-8901-37e80a611f8c"));
         transaction.setProductId(UUID.fromString("658e3ed6-1a43-43b8-98f0-2d23421f65c8"));
         transaction.setProductAmount(15);
         transaction.setPromoCode(null);
@@ -78,59 +76,52 @@ class TransactionRepositoryTest{
 
         transaction.setTotalPrice(34500000.0);
 
-        transactionRepository.create(transaction);
+        transactionRepository.save(transaction);
 
-        Transaction editedTransaction1 = new Transaction(transaction.getUserUsername());
-        editedTransaction1.setId(transaction.getId());
-        editedTransaction1.setProductId(UUID.fromString("035ee4bb-9510-4444-aeea-cec565e91d3c"));
-        editedTransaction1.setProductAmount(20);
-        editedTransaction1.setPromoCode("BELANJAHEMAT20");
-        editedTransaction1.setTotalPrice(5500000.0);
-        transactionRepository.edit(editedTransaction1);
+        Transaction tempTransaction = transactionRepository.findAll().getFirst();
+        tempTransaction.setProductId(UUID.fromString("035ee4bb-9510-4444-aeea-cec565e91d3c"));
+        tempTransaction.setProductAmount(20);
+        tempTransaction.setPromoCode("BELANJAHEMAT20");
+        tempTransaction.setTotalPrice(5500000.0);
+        transactionRepository.save(tempTransaction);
 
-        assertEquals(transaction.getId(), editedTransaction1.getId());
-        assertEquals(transaction.getUserUsername(), editedTransaction1.getUserUsername());
-        assertEquals(UUID.fromString("035ee4bb-9510-4444-aeea-cec565e91d3c"), editedTransaction1.getProductId());
-        assertEquals(20, editedTransaction1.getProductAmount());
-        assertEquals("BELANJAHEMAT20", editedTransaction1.getPromoCode());
-        assertEquals(transaction.getPaymentDate(), editedTransaction1.getPaymentDate());
-        assertEquals(5500000.0, editedTransaction1.getTotalPrice());
+        Transaction editedTransaction = transactionRepository.findAll().getFirst();
+
+        assertEquals(transaction.getUserId(), editedTransaction.getUserId());
+        assertEquals(UUID.fromString("035ee4bb-9510-4444-aeea-cec565e91d3c"), editedTransaction.getProductId());
+        assertEquals(20, editedTransaction.getProductAmount());
+        assertEquals("BELANJAHEMAT20", editedTransaction.getPromoCode());
+        assertEquals(tempTransaction.getPaymentDate(), editedTransaction.getPaymentDate());
+        assertEquals(5500000.0, editedTransaction.getTotalPrice());
     }
 
     @Test
-    void testEditTransactionNotFound(){
-        Transaction transaction = new Transaction("dummyUsername");
-        transaction.setId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+    void testFindById(){
+        Transaction transaction = new Transaction();
+        transaction.setUserId(UUID.fromString("14ed324a-7d0b-45a5-8901-37e80a611f8c"));
         transaction.setProductId(UUID.fromString("658e3ed6-1a43-43b8-98f0-2d23421f65c8"));
         transaction.setProductAmount(15);
         transaction.setPromoCode(null);
-
-        try {
-            transaction.setPaymentDate(this.dateFormat.parse("Mon Apr 22 18:58:25 WIB 2024"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         transaction.setTotalPrice(34500000.0);
 
-        transactionRepository.create(transaction);
+        transactionRepository.save(transaction);
 
-        Transaction editedTransaction1 = new Transaction(transaction.getUserUsername());
-        editedTransaction1.setId(UUID.fromString("b07472cf-4dd0-45e7-84c6-01820af7daad")); // set wrong ID
-        editedTransaction1.setProductId(UUID.fromString("035ee4bb-9510-4444-aeea-cec565e91d3c"));
-        editedTransaction1.setProductAmount(20);
-        editedTransaction1.setPromoCode("BELANJAHEMAT20");
-        editedTransaction1.setTotalPrice(5500000.0);
+        Transaction tempTransaction = transactionRepository.findAll().getFirst();
+        UUID tempTransactionId = tempTransaction.getTransactionId();
 
-        assertThrows(NoSuchElementException.class, () -> {
-            transactionRepository.edit(editedTransaction1);
-        });
+        Transaction foundTransaction = transactionRepository.findById(tempTransactionId).get();
+        assertEquals(transaction.getUserId(), foundTransaction.getUserId());
+        assertEquals(transaction.getProductId(), foundTransaction.getProductId());
+        assertEquals(transaction.getProductAmount(), foundTransaction.getProductAmount());
+        assertEquals(transaction.getPromoCode(), foundTransaction.getPromoCode());
+        assertEquals(transaction.getTotalPrice(), foundTransaction.getTotalPrice());
     }
 
     @Test
     void testDeleteTransactionThenFindById(){
-        Transaction transaction = new Transaction("dummyUsername");
-        transaction.setId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+        Transaction transaction = new Transaction();
+        transaction.setTransactionId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+        transaction.setUserId(UUID.fromString("14ed324a-7d0b-45a5-8901-37e80a611f8c"));
         transaction.setProductId(UUID.fromString("658e3ed6-1a43-43b8-98f0-2d23421f65c8"));
         transaction.setProductAmount(15);
         transaction.setPromoCode(null);
@@ -143,13 +134,13 @@ class TransactionRepositoryTest{
 
         transaction.setTotalPrice(34500000.0);
 
-        transactionRepository.create(transaction);
+        transactionRepository.save(transaction);
 
-        transactionRepository.delete(transaction.getId());
-        UUID id1 = transaction.getId();
+        Transaction tempTransaction = transactionRepository.findAll().getFirst();
+        UUID tempTransactionId = tempTransaction.getTransactionId();
 
-        assertThrows(NoSuchElementException.class, () -> {
-            transactionRepository.findById(id1);
-        });
+        transactionRepository.deleteById(tempTransactionId);
+
+        assertEquals(Optional.empty(), transactionRepository.findById(tempTransactionId));
     }
 }
