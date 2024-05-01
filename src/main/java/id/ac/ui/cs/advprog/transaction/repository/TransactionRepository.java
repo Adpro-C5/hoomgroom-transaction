c7f49f9921651b33271660261221d92992096c71
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-05-01 12:10:09 +0700

    [REFACTOR] use JpaRepository

--- src/main/java/id/ac/ui/cs/advprog/transaction/repository/TransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/transaction/repository/TransactionRepository.java
@@ -1,44 +1,13 @@
 package id.ac.ui.cs.advprog.transaction.repository;
 
 import id.ac.ui.cs.advprog.transaction.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
 @Repository
-public class TransactionRepository{
-    private List<Transaction> transactionData = new ArrayList<>();
-
-    public Transaction create(Transaction transaction){
-        transactionData.add(transaction);
-        return transaction;
-    }
-
-    public Iterator<Transaction> findAll(){
-        return transactionData.iterator();
-    }
-
-    public Transaction findById(UUID transactionId){
-        for(Transaction transaction : transactionData){
-            if(transaction.getId().equals(transactionId)){
-                return transaction;
-            }
-        }
-        throw new NoSuchElementException("Product not found");
-    }
-
-    public void edit(Transaction editedTransaction){
-        Transaction transaction = findById(editedTransaction.getId());
-
-        transaction.setProductId(editedTransaction.getProductId());
-        transaction.setProductAmount(editedTransaction.getProductAmount());
-        transaction.setPromoCode(editedTransaction.getPromoCode());
-        transaction.setPaymentDate(editedTransaction.getPaymentDate());
-        transaction.setTotalPrice(editedTransaction.getTotalPrice());
-    }
-
-    public void delete(UUID transactionId ){
-        Transaction deletedTransaction = findById(transactionId);
-        transactionData.remove(deletedTransaction);
-    }
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    List<Transaction> findAllByUserId(UUID userId);
 }
