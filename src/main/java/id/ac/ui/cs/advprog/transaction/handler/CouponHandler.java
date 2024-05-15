d0b399b4a6191b34f21c9131484ccf74ae2c2331
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-05-15 20:03:29 +0700

    Fetch data for product and promo code with api in handler

--- src/main/java/id/ac/ui/cs/advprog/transaction/handler/CouponHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/transaction/handler/CouponHandler.java
@@ -1,42 +1,64 @@
 package id.ac.ui.cs.advprog.transaction.handler;
 
+import id.ac.ui.cs.advprog.transaction.dto.PromoCodeDTO;
 import id.ac.ui.cs.advprog.transaction.model.Transaction;
-import org.springframework.cglib.core.Local;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CouponHandler extends Handler{
+
+    ResponseEntity<PromoCodeDTO> response;
 
     @Override
     public void handle(Transaction transaction){
 
         LocalDate today = LocalDate.now();
-
-        // dummy coupon expired date
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
-        LocalDate couponExpiredDate = LocalDate.parse("2030-Apr-10", formatter);
+        RestTemplate restTemplate = new RestTemplate();
 
-        // dummy coupon minimum purchase
-        Double couponMinimumPurchase = 1000000.0;
+        if(transaction.getPromoCode() == null){
+            handleNext(transaction);
+            return;
+        }
 
-        // dummy coupon discount
-        Double couponDiscount = 375000.0;
+        try{
+            String getCoupontUrl = "http://localhost:8002/promo_code/name/" + transaction.getPromoCode();
+            response = restTemplate.getForEntity(getCoupontUrl, PromoCodeDTO.class);
+        } catch(Exception e){
+            throw new IllegalArgumentException("Promo code " + transaction.getPromoCode() + " not found");
+        }
+
+        PromoCodeDTO promoCode = response.getBody();
 
         Double purchase = transaction.getTotalPrice();
 
-        // nanti akan ditambahkan satu if lagi untuk memeriksa apakah kode kupon sudah digunakan
-        if(today.isAfter(couponExpiredDate) || today.isEqual(couponExpiredDate)){
-            throw new IllegalArgumentException("Your coupon code has expired by " + couponExpiredDate.format(formatter));
+        if(today.isAfter(promoCode.getExpiredDate()) || today.isEqual(promoCode.getExpiredDate())){
+            throw new IllegalArgumentException("Your coupon code has expired by " + promoCode.getExpiredDate().format(formatter));
         }
-        else if(purchase < couponMinimumPurchase){
-            throw new IllegalArgumentException("Your purchase is " + purchase + " while the minimum purchase for this coupon is " + couponMinimumPurchase);
+        else if(purchase < promoCode.getMinimumPurchase()){
+            throw new IllegalArgumentException("Your purchase is " + purchase + " while the minimum purchase for this coupon is " + promoCode.getMinimumPurchase());
         }
 
-        Double newPrice = purchase - couponDiscount;
+        Double newPrice = purchase * ( (100.0 - getNumberAtEnd(promoCode.getName())) / 100.0);
         transaction.setTotalPrice(newPrice);
 
         handleNext(transaction);
+    }
+
+
+    // credit: chatGPT
+    public static Integer getNumberAtEnd(String input) {
+        // Use a regular expression to find the numeric part at the end of the string
+        Pattern pattern = Pattern.compile("(\\d+)$");
+        Matcher matcher = pattern.matcher(input);
+
+        // If a match is found, return the number, otherwise return null
+        return matcher.find() ? Integer.parseInt(matcher.group(1)) : null;
     }
 }
 
--- src/main/java/id/ac/ui/cs/advprog/transaction/handler/TotalPriceHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/transaction/handler/TotalPriceHandler.java
@@ -1,14 +1,28 @@
 package id.ac.ui.cs.advprog.transaction.handler;
 
+import id.ac.ui.cs.advprog.transaction.dto.ProductDTO;
 import id.ac.ui.cs.advprog.transaction.model.Transaction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class TotalPriceHandler extends Handler{
+
+    RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public void handle(Transaction transaction){
 
-        // dummy product price
-        Double productPrice = 3556000.0;
+        // Sementara menggunakan localhost
+        String getProductUrl = "http://localhost:8002/product/" + transaction.getProductId();
+        ResponseEntity<ProductDTO> response = restTemplate.getForEntity(getProductUrl, ProductDTO.class);
+
+        ProductDTO product = response.getBody();
+
+        if(product == null){
+            throw new IllegalArgumentException("Product with id " + transaction.getProductId() + " not found");
+        }
+
+        Double productPrice = product.getPrice();
 
         Double totalPrice = productPrice * transaction.getProductAmount();
         transaction.setTotalPrice(totalPrice);
