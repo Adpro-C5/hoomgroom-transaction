c756b4e7dfc6abcbe3a9364f38831476ed9bffe8
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-05-30 13:17:03 +0700

    Add cors configuration file

---
+++ src/main/java/id/ac/ui/cs/advprog/transaction/config/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.transaction.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://hoomgroom-frontend-tawny.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
